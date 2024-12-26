547f2cf619f4d7b458d3e16a4346c16d05bd92d5
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-12-26 13:28:51 +0300

    setting up chakra ui and react icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Stack } from '@chakra-ui/react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Stack spacing={4} direction='row' align='center'>
+      <Button 
+        colorScheme='teal' 
+        size='md'
+        _hover={{ bg: 'teal.600' }}
+      >
+        Primary Button
+      </Button>
+
+      <Button
+        variant='outline'
+        colorScheme='purple'
+        size='md'
+        _hover={{ bg: 'purple.50' }}
+      >
+        Secondary Button
+      </Button>
+    </Stack>
   )
 }
 
